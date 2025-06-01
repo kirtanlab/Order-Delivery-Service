b7d5d93430053457cdf0532723b6515067ae61ac
kirtanlab <kirtanprajapati0804@gmail.com>
2025-06-01 17:32:05 -0400

    log updated

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func JSONMiddleware() gin.HandlerFunc {
 func initEnv() {
 	err := dotEnv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Warning: .env file not found, relying on environment variables")
 	}
 }
 
